0bedf61fd4a746f910147c7b90b0ee6d6e24a61c
Mariano Benedettini <mariano.benedettini@devecoop.com>
2014-08-15 18:00:43 -0300

    Introduce Client class

--- ejemplo1.py
+++ ejemplo1.py
@@ -20,6 +20,29 @@ class Cuenta:
         print "El saldo de la cuenta es de %s" % self._saldo
 
 
+class Cliente:
+
+    def __init__(self, nombre, apellido):
+        self._nombre = nombre
+        self._apellido = apellido
+        self._cuenta = Cuenta()
+
+
+    def depositar(self, monto):
+        self._cuenta.depositar(monto)
+
+    def extraer(self, monto):
+        self._cuenta.extraer(monto)
+
+    def imprimirSaldo(self):
+        print "Mostrando el saldo de %s" % self._nombre
+        self._cuenta.imprimirSaldo()
+
+    def quienSos(self):
+        print "Soy %s %s" % (self._nombre, self._apellido)
+        self.imprimirSaldo()
+        
+        
 # Codigo necesario para ejecutar el ejemplo
 if __name__ == "__main__" :
     miCuenta = Cuenta()
